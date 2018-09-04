03511d785380b95e3f765b25a9c2bfd5c50f437f
Anay Wadhera <anay1018@gmail.com>
2018-09-03 17:12:56 -0700

    vendorsetup: Adapt for wrath
    - Add Our Devices!
    
    Co-authored-by: Alex Koskovich <zvnexus@outlook.com>
    Signed-off-by: iamtoxyc <vtechnical1804@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,10 @@
-aosp_devices=('dragon' 'marlin' 'shamu' 'taimen')
-caf_devices=('cheeseburger' 'dumpling' 'nash' 'oneplus3' 'potter' 'tenderloin')
+devices=('beryllium' 'raphael' 'lavender' 'violet' 'mido' 'ginkgo' 'RMX1971' )
 
 function lunch_devices() {
     add_lunch_combo wrath_${device}-user
     add_lunch_combo wrath_${device}-userdebug
 }
 
-if [[ $( grep -i "caf" manifest/README.md) ]]; then
-    for device in ${caf_devices[@]}; do
-        lunch_devices
-    done
-else
-    for device in ${aosp_devices[@]}; do
-        lunch_devices
-    done
-fi
+for device in ${devices[@]}; do
+    lunch_devices
+done
