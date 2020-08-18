PotatoProject/vendor_potato

vendor: Add vendorsetup and include supported devices
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

vendorsetup.sh
@@ -1,0 +1,3 @@
+for device in $(cat $(gettop)/vendor/potato/potato.devices | sed -e 's/#.*$//' | awk '{printf "potato_%s-userdebug\n", $1}'); do
+    add_lunch_combo $device;
+done;

WrathAosp/android_vendor_wrath

Signed-off-by: iamtoxyc <vtechnical1804@gmail.com>

vendorsetup.sh
@@ -1,0 +1,3 @@
+for device in $(cat $(gettop)/vendor/wrath/wrath.devices | sed -e 's/#.*$//' | awk '{printf "wrath_%s-userdebug\n", $1}'); do
+    add_lunch_combo $device;
+done;