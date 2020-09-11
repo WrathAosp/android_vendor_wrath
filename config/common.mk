CherishOS/android_vendor_cherish

themes: Switch Styles [3/3]
- Original credits to @SKULSHADY aka Havoc-OS

Signed-off-by: Aston-Martinn <advaithbhat9@gmail.com>

config/common.mk
@@ -186,6 +186,6 @@ include vendor/pixelstyle/config.mk
 include vendor/google-customization/config.mk
 
 # Include Cherish theme files
-include vendor/cherish/themes/backgrounds/themes.mk
+include vendor/cherish/themes/themes.mk
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk

WrathAosp/android_vendor_wrath

Signed-off-by: iamtoxyc <vtechnical1804@gmail.com>

config/common.mk
@@ -12,7 +12,7 @@ include vendor/Rage/config.mk
 include vendor/wrath/bootanimation/bootanimation.mk
 
 # Include Wrath Theme
-include vendor/wrath/themes/backgrounds/themes.mk
+include vendor/wrath/themes/themes.mk
 
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -107,6 +107,7 @@ PRODUCT_PACKAGES += \
     PixelSetupWizardOverlay \
     PixelSetupWizardAodOverlay
 
+# Faceunlock
 $(call inherit-product-if-exists, external/motorola/faceunlock/config.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk