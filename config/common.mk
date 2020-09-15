xK4m3l/platform_vendor_streak

vendor: Port FaceUnlock [2/4]
also build only on official builds

Signed-off-by: HSgod <hsgodkontakt@gmail.com>

config/common.mk
@@ -110,5 +110,17 @@ include vendor/streak/config/bootanimation.mk
 # Packages
 include vendor/streak/config/packages.mk
 
+# Face Unlock
+ifeq ($(STREAK_BUILD_TYPE), OFFICIAL)
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+endif
+
 # Versioning
 include vendor/streak/config/version.mk

WrathAosp/android_vendor_wrath

config/common.mk
@@ -112,3 +112,15 @@ $(call inherit-product-if-exists, external/motorola/faceunlock/config.mk)
 
 -include $(WORKSPACE)/build_env/image-auto-bits.mk
 -include vendor/wrath/config/partner_gms.mk
+
+# Face Unlock
+ifeq ($(WRATH_BUILD_TYPE), OFFICIAL)
+TARGET_FACE_UNLOCK_SUPPORTED := false
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+endif